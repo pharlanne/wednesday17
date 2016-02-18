c944a976b1a8aecb919d1a995db9730d75b0c950
Anne Brooks <pharlanne@googlemail.com>
2016-02-18 08:49:29 +0000

    resubmitting

--- tdd_function.rb
+++ tdd_function.rb
@@ -1,46 +1,39 @@
-# def add_length(array1,array2)
-#   add_result = array1.length + array2.length
-#     return add_result
-# end
+def add_length(array1,array2)
+  add_result = array1.length + array2.length
+    return add_result
+end
 
 
-# def sum_array(array)
-#   array_tot = 0
-#     for i in array
-#     array_tot = i + array_tot
-#     end
-#   return array_tot
-# end
+def sum_array(array)
+  array_tot = 0
+    for i in array
+     array_tot = i + array_tot
+    end
+  return array_tot
+end
 
 
-#   def find_item(i, array)
-#     for x in array
-#       if x == i
-#         return true
-#       end
-#     end
-#   end
+  def find_item(i, array)
+    for x in array
+      if x == i
+        return true
+      end
+    end
+  end
 
-#   def first_key_name (hash)
-#     key_name = hash.keys[0]
-#     return key_name
-#   end
+  def first_key_name (hash)
+    key_name = hash.keys[0]
+      return key_name
+  end
 
-  # def capitals(countries) 
-  #   for i in countries.keys
-  #     puts countries[i][:capital]
-  #   end
-  # end
+  def capitals(countries) 
+    for i in countries.keys
+      puts countries[i][:capital]
+    end
+  end
   
-  my_hash = {0 => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
-   my_hash[:four] = 4
-   puts my_hash
-  # How would you return the string "One"?
-  # How would you return the string "Two"?
-  # How would you return the number 2?
-  # How would you add {3 => "Three"} to the hash?
-  # How would you add {:four => 4} to the hash?
+
 
 
 
